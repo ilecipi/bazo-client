f6c39f4a5f18c6c227a5f5c23339c1764e777f1f
Marc-Alain Chételat <m.chetelat@me.com>
2018-03-04 14:05:33 +0100

    Refactor connection management

--- client/p2p.go
+++ client/p2p.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"errors"
+	"fmt"
 	"github.com/bazo-blockchain/bazo-miner/p2p"
 	"github.com/bazo-blockchain/bazo-miner/protocol"
 	"github.com/bazo-blockchain/bazo-miner/storage"
@@ -18,186 +19,183 @@ const (
 )
 
 func reqBlock(blockHash [32]byte) (block *protocol.Block) {
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
 
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+		packet := p2p.BuildPacket(p2p.BLOCK_REQ, blockHash[:])
+		conn.Write(packet)
 
-	packet := p2p.BuildPacket(p2p.BLOCK_REQ, blockHash[:])
-	conn.Write(packet)
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID != p2p.BLOCK_RES {
+			logger.Printf("Requesting block failed.")
+			return
+		}
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return
-	}
-
-	if header.TypeID == p2p.BLOCK_RES {
 		block = block.Decode(payload)
-	}
 
-	conn.Close()
+		conn.Close()
+	}
 
 	return block
 }
 
 func reqTx(txType uint8, txHash [32]byte) (tx protocol.Transaction) {
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
 
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+		packet := p2p.BuildPacket(txType, txHash[:])
+		conn.Write(packet)
 
-	packet := p2p.BuildPacket(txType, txHash[:])
-	conn.Write(packet)
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil {
+			logger.Printf("Requesting tx failed.")
+			return
+		}
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return
+		switch header.TypeID {
+		case p2p.ACCTX_RES:
+			var accTx *protocol.AccTx
+			accTx = accTx.Decode(payload)
+			tx = accTx
+		case p2p.CONFIGTX_RES:
+			var configTx *protocol.ConfigTx
+			configTx = configTx.Decode(payload)
+			tx = configTx
+		case p2p.FUNDSTX_RES:
+			var fundsTx *protocol.FundsTx
+			fundsTx = fundsTx.Decode(payload)
+			tx = fundsTx
+		case p2p.STAKETX_RES:
+			var stakeTx *protocol.StakeTx
+			stakeTx = stakeTx.Decode(payload)
+			tx = stakeTx
+		}
+
+		conn.Close()
 	}
-
-	switch header.TypeID {
-	case p2p.ACCTX_RES:
-		var accTx *protocol.AccTx
-		accTx = accTx.Decode(payload)
-		tx = accTx
-	case p2p.CONFIGTX_RES:
-		var configTx *protocol.ConfigTx
-		configTx = configTx.Decode(payload)
-		tx = configTx
-	case p2p.FUNDSTX_RES:
-		var fundsTx *protocol.FundsTx
-		fundsTx = fundsTx.Decode(payload)
-		tx = fundsTx
-	case p2p.STAKETX_RES:
-		var stakeTx *protocol.StakeTx
-		stakeTx = stakeTx.Decode(payload)
-		tx = stakeTx
-	}
-
-	conn.Close()
 
 	return tx
 }
 
 func reqIntermediateNodes(blockHash [32]byte, txHash [32]byte) (nodes [][32]byte) {
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
+		var data [][]byte
+		data = append(data, blockHash[:])
+		data = append(data, txHash[:])
 
-	packet := p2p.BuildPacket(p2p.INTERMEDIATE_NODES_REQ, protocol.SerializeSlice32([][32]byte{blockHash, txHash}))
-	conn.Write(packet)
+		packet := p2p.BuildPacket(p2p.INTERMEDIATE_NODES_REQ, protocol.Encode(data, 32))
+		conn.Write(packet)
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID != p2p.INTERMEDIATE_NODES_RES {
+			logger.Printf("Requesting intermediate nodes failed.")
+			return
+		}
+
+		for _, data := range protocol.Decode(payload, 32) {
+			var node [32]byte
+			copy(node[:], data)
+			nodes = append(nodes, node)
+		}
+
+		conn.Close()
 	}
-
-	if header.TypeID == p2p.INTERMEDIATE_NODES_RES {
-		return protocol.DeserializeSlice32(payload)
-	}
-
-	conn.Close()
 
 	return nil
 }
 
 func reqBlockHeader(blockHash []byte) (blockHeader *protocol.Block) {
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
 
-	packet := p2p.BuildPacket(p2p.BLOCK_HEADER_REQ, blockHash)
-	conn.Write(packet)
+		packet := p2p.BuildPacket(p2p.BLOCK_HEADER_REQ, blockHash)
+		conn.Write(packet)
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return
-	}
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID != p2p.BlOCK_HEADER_RES {
+			logger.Printf("Requesting block header failed.")
+			return
+		}
 
-	if header.TypeID == p2p.BlOCK_HEADER_RES {
 		blockHeader = blockHeader.DecodeHeader(payload)
-	}
 
-	conn.Close()
+		conn.Close()
+	}
 
 	return blockHeader
 }
 
 func ReqAcc(accountHash [32]byte) (acc *protocol.Account) {
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
 
-	packet := p2p.BuildPacket(p2p.ACC_REQ, accountHash[:])
-	conn.Write(packet)
+		packet := p2p.BuildPacket(p2p.ACC_REQ, accountHash[:])
+		conn.Write(packet)
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return nil
-	}
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID != p2p.ACC_RES {
+			logger.Printf("Requesting account failed.")
+			return nil
+		}
 
-	if header.TypeID == p2p.ACC_RES {
 		acc = acc.Decode(payload)
-	}
 
-	conn.Close()
+		conn.Close()
+	}
 
 	return acc
 }
 
 func reqRootAcc(accountHash [32]byte) (rootAcc *protocol.Account) {
-	conn := Connect(storage.BOOTSTRAP_SERVER)
+	if conn := p2p.Connect(storage.BOOTSTRAP_SERVER); conn != nil {
 
-	packet := p2p.BuildPacket(p2p.ROOTACC_REQ, accountHash[:])
-	conn.Write(packet)
+		packet := p2p.BuildPacket(p2p.ROOTACC_REQ, accountHash[:])
+		conn.Write(packet)
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return nil
-	}
+		_, payload, err := p2p.RcvData(conn)
+		if err != nil {
+			logger.Printf("Requesting root account failed.")
+			return nil
+		}
 
-	if header.TypeID == p2p.ROOTACC_RES {
 		rootAcc = rootAcc.Decode(payload)
-	}
 
-	conn.Close()
+		conn.Close()
+	}
 
 	return rootAcc
 }
 
-func reqNonVerifiedTx(addressHash [32]byte) (openFundsTxs []*protocol.FundsTx) {
-	conn := Connect(MULTISIG_SERVER)
+func reqNonVerifiedTx(addressHash [32]byte) (nonVerifiedTxs []*protocol.FundsTx) {
+	if conn := p2p.Connect(MULTISIG_SERVER); conn != nil {
+		packet := p2p.BuildPacket(p2p.FUNDSTX_REQ, addressHash[:])
+		conn.Write(packet)
 
-	packet := p2p.BuildPacket(p2p.FUNDSTX_REQ, addressHash[:])
-	conn.Write(packet)
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID != p2p.FUNDSTX_RES {
+			logger.Printf("Requesting non verified tx failed.")
+			return nil
+		}
 
-	header, payload, err := RcvData(conn)
-	if err != nil {
-		logger.Printf("Disconnected: %v\n", err)
-		return nil
-	}
-
-	if header.TypeID == p2p.FUNDSTX_RES {
-		index := 0
-		cnt := 1
-		for len(payload) >= cnt*protocol.FUNDSTX_SIZE {
-			var openFundsTx *protocol.FundsTx
-			openFundsTx = openFundsTx.Decode(payload[index : index+protocol.FUNDSTX_SIZE])
-			openFundsTxs = append(openFundsTxs, openFundsTx)
-
-			index += protocol.FUNDSTX_SIZE
-			cnt++
+		for _, data := range protocol.Decode(payload, protocol.FUNDSTX_SIZE) {
+			var tx *protocol.FundsTx
+			nonVerifiedTxs = append(nonVerifiedTxs, tx.Decode(data))
 		}
 	}
 
-	return openFundsTxs
+	return nonVerifiedTxs
 }
 
 func SendTx(dial string, tx protocol.Transaction, typeID uint8) (err error) {
-	conn := Connect(dial)
-	packet := p2p.BuildPacket(typeID, tx.Encode())
-	conn.Write(packet)
+	if conn := p2p.Connect(dial); conn != nil {
+		packet := p2p.BuildPacket(typeID, tx.Encode())
+		conn.Write(packet)
 
-	header, payload, err := RcvData(conn)
-	if header.TypeID == p2p.NOT_FOUND {
-		err = errors.New(string(payload[:]))
+		header, payload, err := p2p.RcvData(conn)
+		if err != nil || header.TypeID == p2p.NOT_FOUND {
+			err = errors.New(string(payload[:]))
+		}
+		conn.Close()
+
+		return err
 	}
-	conn.Close()
 
-	return err
+	txHash := tx.Hash()
+	return errors.New(fmt.Sprintf("Sending tx %x failed.", txHash[:8]))
 }
