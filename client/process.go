3e9ec2f096932d9c861f8900672f197f918db744
Roman Blum <rmnblm@icloud.com>
2018-10-25 08:55:23 +0200

    WIP: remove code relevant to seeds/hashedseeds

--- client/process.go
+++ client/process.go
@@ -187,12 +187,9 @@ func parseFundsTx(args []string) (tx protocol.Transaction, err error) {
 }
 
 func parseStakeTx(args []string) (tx protocol.Transaction, err error) {
-	stakeTxUsage := "\nUsage: bazo_client stakeTx <header> <fee> <isStaking> <account> <privKey> <commPrivKey>"
+	stakeTxUsage := "\nUsage: bazo_client stakeTx <header> <fee> <isStaking> <account> <privKey> <commitmentFile>"
 
-	var (
-		accountPubKey [64]byte
-		hashedSeed    [32]byte
-	)
+	var accountPubKey [64]byte
 
 	if len(args) != 6 {
 		return nil, errors.New(fmt.Sprintf("%v%v", ARGS_MSG, stakeTxUsage))
@@ -211,19 +208,6 @@ func parseStakeTx(args []string) (tx protocol.Transaction, err error) {
 	isStaking, err := strconv.Atoi(args[2])
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("%v%v", err, stakeTxUsage))
-	}
-
-	//create new seed if node wants to stake
-	//seed file cannot be simply overwritten since in case of a rollback
-	//the validator must also be able to access an old seed
-	if isStaking != 0 {
-		//generate random seed and store it
-		seed := protocol.CreateRandomSeed()
-
-		//create the hash of the seed which will be included in the transaction
-		hashedSeed = protocol.SerializeHashContent(seed)
-
-		storage.AppendNewSeed(args[4]+"_seed.json", storage.SeedJson{fmt.Sprintf("%x", string(hashedSeed[:])), string(seed[:])})
 	}
 
 	hashFromFile, err := os.Open(args[3])
@@ -266,10 +250,9 @@ func parseStakeTx(args []string) (tx protocol.Transaction, err error) {
 		byte(header),
 		uint64(fee),
 		isStakingAsBool,
-		hashedSeed,
 		protocol.SerializeHashContent(accountPubKey[:]),
 		&privKey,
-		&commPrivKey.PublicKey,
+		&commPrivKey,
 	)
 
 	if err != nil {
